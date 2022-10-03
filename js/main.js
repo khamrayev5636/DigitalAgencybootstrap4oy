c6dbdfe58e34ec6113190ec97b59b14ca855e02d
khamrayev5636 <khamrayev5636@gmail.com>
2022-10-03 22:12:09 +0500

    Finished second day exam

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ burgerBtn.addEventListener("click" , () =>{
     burgerBtn.classList.toggle("shownav");
     navMenyu.classList.toggle("shownav");
 })
+
+
+$('.card__carousel').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                centerMode: true,
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
